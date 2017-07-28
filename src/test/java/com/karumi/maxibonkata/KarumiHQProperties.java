39899b9c1ea5f9b6572d3a2ea11ae50b42aa4bda
David A. Velasco <dvelasco@solidgear.es>
2017-07-28 14:17:00 +0200

    [GREEN] Test number of maxibons always greater than two

---
+++ src/test/java/com/karumi/maxibonkata/KarumiHQProperties.java
@@ -0,0 +1,30 @@
+package com.karumi.maxibonkata;
+
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by David A. Velasco on 28/7/17.
+ */
+
+
+@RunWith(JUnitQuickcheck.class)
+public class KarumiHQProperties {
+
+    @Property public void theNumberOfMaxibonsIsGreaterThanTwo(
+        @From(DevelopersGenerator.class) Developer developer
+    ) {
+        System.out.println("-----------------> " + developer);
+
+        KarumiHQs hq = new KarumiHQs();
+        hq.openFridge(developer);
+
+        assertTrue(hq.getMaxibonsLeft() > 2);
+    }
+
+}
